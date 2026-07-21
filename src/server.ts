feca5773790612587561ba3ff0e1876021abd1d4
Pawel <paweljonca94@gmail.com>
2026-07-21 17:52:11 +0200

    Remove broken SSR entry and use static deploy

--- src/server.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  async fetch(request: Request, env: unknown, ctx: unknown) {
-    const handler = await import("./assets/server-CnfIKFS-.js");
-    const mod = handler.default ?? handler;
-    return mod.fetch(request, env, ctx);
-  },
-};
